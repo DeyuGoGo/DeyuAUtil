0bd53bf1ed0a105692fabae40c756470a60bd8e7
deyu <a0924xxx@gmail.com>
2015-05-16 13:49:37 +0800

    Initial commit

---
+++ util/src/main/java/go/deyu/util/AppContextSingleton.java
@@ -0,0 +1,24 @@
+package go.deyu.util;
+
+import android.content.Context;
+
+public class AppContextSingleton {
+    private static AppContextSingleton INSTANCE;
+    
+    private Context appContext;
+    
+    private AppContextSingleton(Context context){
+        this.appContext = context;
+    }
+    
+    public static synchronized AppContextSingleton initialize(Context context){
+        if (INSTANCE == null) {
+            INSTANCE = new AppContextSingleton(context.getApplicationContext());
+        }
+        return INSTANCE;
+    }
+    
+    public static Context getApplicationContext() {
+        return INSTANCE.appContext;
+    }
+}
